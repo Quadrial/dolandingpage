cc8cbda3986ec10bab5dc005459596e4bc1e6ecd
Quadrial <opequadri2@gmail.com>
2025-09-09 10:43:30 +0100

    production solution

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+// import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
